7a0b66d9e765eed17571e75e70f4fec02b8d4df8
Alex Kruse <kruse-alex@outlook.de>
2018-04-23 17:54:42 +0200

    Update processing.R

--- processing.R
+++ processing.R
@@ -1,19 +1,79 @@
+# load packages
+require(rgeos)
+require(sp)
+require(rgdal)
+require(tidyverse)
+require(geosphere)
+require(chron)
+require(lubridate)
+require(tmaptools)
+require(gganimate)
+
 # import route
 setwd("C:/Users/akruse/Downloads/")
-import <- ogrListLayers("C:/Users/akruse/Downloads/mari.kml")
-l2 = readOGR("mari.kml","Directions from KarolinenstraÃŸe 34-35, 20357 Hamburg, Germany to KarolinenstraÃŸe 35, 20357 Hamburg, Germany",require_geomType="wkbLineString")
+import <- ogrListLayers("C:/Users/akruse/Downloads/testii.kml")
+l2 = readOGR("testii.kml",import[1],require_geomType="wkbLineString")
 
-# get coords from spatial file
-res <- lapply(slot(l2, "lines"), function(x) lapply(slot(x, "Lines"),
-                                                          function(y) slot(y, "coords")))
-# format to dataframe
-res = as.data.frame(res)
+
+# import finish line
+setwd("C:/Users/akruse/Downloads/")
+import <- ogrListLayers("C:/Users/akruse/Downloads/testoo.kml")
+l3 = readOGR("testoo.kml",import[1],require_geomType="wkbLineString")
+l3 = double_line(l3, width = .0003, sides = "left")
+
+
+# combine routes
+l2 = rbind(l2, l3)
+
+start = 53.55843, 9.97466 53.55854, 9.97558
+end = 53.5587, 9.97537
+
+# create finish line
+l1 <- cbind(c(9.97466, 9.97558), c(53.55843, 53.55854))
+Sl1 <- Line(l1)
+S1 <- Lines(list(Sl1), ID = "a")
+Sl <- SpatialLines(list(S1))
+df <- data.frame(len = sapply(1:length(Sl), function(i) gLength(Sl[i, ])))
+rownames(df) <- sapply(1:length(Sl), function(i) Sl@lines[[i]]@ID)
+Sldf <- SpatialLinesDataFrame(Sl, data = df)
+oyoy =aggregate(rbind(Sldf,l2))
+
+# offset lines
+res2 = double_line(l2, width = .00003, sides = "left")
+res3 = double_line(l2, width = .00004, sides = "left")
+res4 = double_line(l2, width = .00005, sides = "left")
+res5 = double_line(l2, width = .00006, sides = "left")
+res6 = double_line(l2, width = .00007, sides = "left")
+res7 = double_line(l2, width = .00008, sides = "left")
+res8 = double_line(l2, width = .00009, sides = "left")
+res9 = double_line(l2, width = .00010, sides = "left")
+
+# convert offset lines to dataframe
+res1 = fortify(l2)
+res2 = fortify(res2)
+res3 = fortify(res3)
+res4 = fortify(res4)
+res5 = fortify(res5)
+res6 = fortify(res6)
+res7 = fortify(res7)
+res8 = fortify(res8)
+res9 = fortify(res9)
+
+# loop over all offset line
+them_res = list(res1,res2,res3,res4,res5,res6,res7,res8,res9)
+datalistall = list()
+its = 1
+for (h in them_res) {
+
+# select offset line dataframe
+res = h
+res = select(res, long, lat)
 
 # loop to get distance between coords
 datalist = list()
 for (i in 1:nrow(res)) {
   
-  datalist[[i+1]] <- distm(c(res$X1[i], res$X2[i]), c(res$X1[i+1], res$X2[i+1]), fun = distHaversine)
+  datalist[[i+1]] <- distm(c(res$long[i], res$lat[i]), c(res$long[i+1], res$lat[i+1]), fun = distHaversine)
 
 }
 datalist[[1]] = 0
@@ -24,18 +84,18 @@ res$dist = big_data
 # add more coords for precision and interpolation
 repeat{
 for (i in 2:nrow(res)) {
-  if(res$dist[i] >= 20){
+  if(res$dist[i] >= 500){
     
-    savory = as.data.frame(midPoint(c(res$X1[i],res$X2[i]),c(res$X1[i-1],res$X2[i-1]))[1])
-    savory$lat = midPoint(c(res$X1[i],res$X2[i]),c(res$X1[i-1],res$X2[i-1]))[2]
-    colnames(savory) = c("X1","X2")
+    savory = as.data.frame(midPoint(c(res$long[i],res$lat[i]),c(res$long[i-1],res$lat[i-1]))[1])
+    savory$lat = midPoint(c(res$long[i],res$lat[i]),c(res$long[i-1],res$lat[i-1]))[2]
+    colnames(savory) = c("long","lat")
     savory$dist = NA
     res <- rbind(res[1:i-1,],savory,res[-(1:i-1),])
     
     # calculate distance again with new coords
     datalist = list()
     for (i in 1:nrow(res)) {
-      datalist[[i+1]] <- distm(c(res$X1[i], res$X2[i]), c(res$X1[i+1], res$X2[i+1]), fun = distHaversine)
+      datalist[[i+1]] <- distm(c(res$long[i], res$lat[i]), c(res$long[i+1], res$lat[i+1]), fun = distHaversine)
     }
     datalist[[1]] = 0
     big_data = do.call(rbind, datalist)
@@ -43,140 +103,65 @@ for (i in 2:nrow(res)) {
     res$dist = big_data
   }
 }
-  if (max(res$dist, na.rm = T) < 20) break
+  if (max(res$dist, na.rm = T) < 500) break
 }
 
 # add cumsum to dataframe
 res$dist_cum = cumsum(res$dist)
 res$dist_max = res$dist_cum/max(res$dist_cum)
 
-# split route for offsetting
-res1 = res[1:1180,]
-res2 = res[1200:1335,]
-res3 = res[1350:3214,]
-
-rownames(res1) = 1:nrow(res1)
-res1$ID = 1
-res1$dist = NULL
-res1 = dplyr::select(res1, X1, X2, ID)
-colnames(res1) = c("x","y","id")
-
-coordinates(res1) <- ~x+y
-x <- lapply(split(res1, res1$id), function(x) Lines(list(Line(coordinates(x))), x$id[1L]))
-lines <- SpatialLines(x)
-data <- data.frame(id = unique(res1$id))
-rownames(data) <- data$id
-l <- SpatialLinesDataFrame(lines, data)
-
-# offset line
-res1_off = offset_line(l2, offset=c(.0005,0,-.0005,-.001))
-res2 = double_line(l, width = .0005, sides = "right")
-
-ggplot() +
-  geom_path(data = df, aes(x = x, y = y)) +
-  geom_path(data = res2, aes(x = X1, y = X2)) +
-  geom_path(data = res3, aes(x = X1, y = X2))
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# read csv
+# read in runners data
 data = read.csv("Läufer_20180418_110452.csv")
-data = data[sample(nrow(data), 100), ]
+data = data[seq(its,nrow(data),9),]
 data$Brutto = chron(times = as.character(data$Brutto))
 data$seconds = period_to_seconds(hms(data$Brutto))
 data = select(data, Name, Brutto, seconds)
 max_run = round(max(data$seconds)/60, 0)
 
-
-
+# position of runners for every 100th second
 datalist = list()
-for (i in seq(1,15000, 10)) {
+for (i in seq(1,15000, 100)) {
 
   data$nach60 = i/data$seconds
   for (j in 1:nrow(data)) {
-    data$lon[j] = res$X1[which.min(abs(res$dist - data$nach60[j]))]
-    data$lat[j] = res$X2[which.min(abs(res$dist - data$nach60[j]))]
+    data$lon[j] = res$long[which.min(abs(res$dist_max - data$nach60[j]))]
+    data$lat[j] = res$lat[which.min(abs(res$dist_max - data$nach60[j]))]
     data$pasted_secs = i
   }
   datalist[[i]] <- data
 }
 
 big_data = do.call(rbind, datalist)
+datalistall[[i]] <- big_data
+its = its+1
+}
+all_data = do.call(rbind, datalistall)
+
+
+
+
+
+ggplotly(ggplot() +
+           geom_path(data = res1, aes(x = long, y = lat), size = 3) +
+           geom_path(data = res1, aes(x = long, y = lat, colour = "red"), size = 0.3) +
+           geom_path(data = res2, aes(x = long, y = lat, colour = "red"), size = 0.3) +
+           geom_path(data = res3, aes(x = long, y = lat, colour = "red"), size = 0.3) +
+           geom_path(data = res4, aes(x = long, y = lat, colour = "red"), size = 0.3) +
+           geom_path(data = res6, aes(x = long, y = lat, colour = "red"), size = 0.3) +
+           geom_path(data = res7, aes(x = long, y = lat, colour = "red"), size = 0.3) +
+           geom_path(data = res8, aes(x = long, y = lat, colour = "red"), size = 0.3) +
+           geom_path(data = res9, aes(x = long, y = lat, colour = "red"), size = 0.3))
+
+
+
+
+
 
 
 
 p = ggplot(res) +
-  geom_path(aes(X1,X2), color = "grey") +
-  geom_point(data = big_data, aes(x = lon, y = lat, frame = pasted_secs)) +
+  geom_path(aes(long,lat), color = "black", size = 3) +
+  geom_point(data = all_data, aes(x = lon, y = lat, frame = pasted_secs), colour = "red", size = 0.2) +
   theme_void()
 
 gganimate(p,"puit.mp4", interval = 1/10, nmax = 100000000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-require(rgeos)
-require(sp)
-require(rgdal)
-require(tidyverse)
-
-# import route
-setwd("C:/Users/akruse/Downloads/")
-import <- ogrListLayers("C:/Users/akruse/Downloads/mari.kml")
-l2 = readOGR("mari.kml","Directions from KarolinenstraÃŸe 34-35, 20357 Hamburg, Germany to KarolinenstraÃŸe 35, 20357 Hamburg, Germany",require_geomType="wkbLineString")
-
-# create polygon from lines with gbuffer
-proj4string(l2) <- CRS("+proj=longlat")
-l2.trans <- spTransform(l2, CRS("+proj=longlat"))
-poly = gBuffer(l2.trans, width=.00025, quadsegs=5, capStyle="ROUND", joinStyle="ROUND", mitreLimit=0.01)
-
-# extract outer lines from polygon
-p.lines = fortify(poly)
-p.lines.1 = p.lines[10:1380,]
-p.lines.2 = p.lines[605:1240,]
-
-# create polygon from lines with gbuffer
-proj4string(l2) <- CRS("+proj=longlat")
-l2.trans <- spTransform(l2, CRS("+proj=longlat"))
-poly2 = gBuffer(l2.trans, width=.0005, quadsegs=5, capStyle="ROUND", joinStyle="ROUND", mitreLimit=0.01)
-
-# extract outer lines from polygon
-p.lines2 = fortify(poly2)
-p.lines2.1 = p.lines2[5:650,]
-p.lines2.2 = p.lines2[655:1390,]
-
-ggplot() +
-  geom_path(data = p.lines.1, aes(x = long, y = lat)) +
-  #geom_path(data = p.lines.2, aes(x = long, y = lat)) +
-  #geom_path(data = p.lines2.1, aes(x = long, y = lat)) +
-  #geom_path(data = p.lines2.2, aes(x = long, y = lat)) +
-  #geom_path(data = l2.fort, aes(x = long, y = lat)) +
-  geom_point(data = p.lines, aes(x = long[2400], y = lat[2400]), color = "red")
-
